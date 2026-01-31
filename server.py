7e4f35ef01ae729949b42d8deccaac6f39241213
japplebey <japplebey@hotmail.com>
2026-01-31 12:06:34 -0500

    Task 6 Completed

--- server.py
+++ server.py
@@ -4,6 +4,12 @@ from EmotionDetection import emotion_detector
 #Initiate the flask app : Emotion Detector
 app = Flask("Emotion Detector")
 
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+
 @app.route("/emotionDetector")
 def get_emotions():
     # Retrieve the text to analyze from the request arguments
@@ -17,34 +23,29 @@ def get_emotions():
     return output
 
 
-@app.route("/")
-def render_index_page():
-    return render_template('index.html')
-
-
 def format_output(emotions):
     dominant_emotion = emotions['dominant_emotion']
 
     # Get number of entries in dictionary
     emotions_length = len(emotions);
- 
-    # Build output
+
+    # Build output string
     count = 0
     output = "For the given statement, the system response is "
     for key, value in emotions.items():
-        ++count
-        fmt = ""
+        count += 1
         if (key != 'dominant_emotion'):
             if (count == (emotions_length-1)):
-                fmt = 'and \'' + key + '\': ' + str(value) + '. '
-            elif (count == emotions_length):
-                fmt = 'The dominant emotion is ' + dominant_emotion + '.'
+                output = output[:-2]   # take out extra comma
+                fmt = ' and \'' + key + '\': ' + str(value) + '. '
             else:                
-                a = '\'' + key + '\': ' + str(value) + ', '
-        output = output + fmt
+                fmt = '\'' + key + '\': ' + str(value) + ', '
+            output = output + fmt
 
-    print(output)        
+    output = output + 'The dominant emotion is ' + dominant_emotion + '.'
+
     return output
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
